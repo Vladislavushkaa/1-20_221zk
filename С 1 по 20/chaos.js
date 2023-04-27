7c98b51c8b86a1948cf9d7dfc8caf149de724431
Vladislavushkaa <131970800+Vladislavushkaa@users.noreply.github.com>
2023-04-27 14:38:57 +0300

    Add files via upload

---
+++ С 1 по 20/chaos.js
@@ -0,0 +1,89 @@
+"use strict" // комментарий
+alert("Какой-то текст");
+/*
+Многострочный
+*/
+let num = 123;
+alert(num);
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+a = 1 + 2 + 3;
+alert(a);
+let b;
+a = 10;
+b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+let c = 10;
+let d = 5;
+let result;
+alert(c + d);
+a = 1;
+b = 2;
+c = 3;
+alert(a + b + c);
+a = 10;
+b = 5;
+c = a - b;
+d = 7;
+alert(result = c + d);
+
+
+// дальнейшие задания были на решения в голове без записи, так что идем дальше
+
+
+a = 1.5;
+b = 0.75;
+alert(a + b);
+a = -100;
+alert(a);
+a = -100;
+alert(+100);
+a = 13;
+b = 5;
+alert(a % b);
+alert(2 ** 10);
+
+
+// дальнейшие задания были на решения в голове без записи, так что идем дальше
+
+
+let str = "Борисов";
+alert(str);
+str = "!!!";
+alert(str);
+let str1 = "java";
+let str2 = "script";
+alert(str1 + str2);
+str1 = "hello";
+str2 = " world";
+alert(str1 + str2);
+alert("wda dwqe".length);  // выведет 8
+str1 = "xxx";
+str2 = "yyy";
+let txt = `aaa + ${str1} + bbb + ${str2} + ccc`;
+alert(txt);
+str = `"a"
+"b"
+"c"`;
+alert(str);
+a = undefined;
+alert(a);
+a = null;
+alert(a);
+a = true;
+alert(a);
+a = false;
+alert(a);
+str = "abc";
+str1 = "kjh";
+alert(str * str1);
+alert(10/0);
+alert(-10/0);
+const PI = 3.14;
+//предположим, что радиус - 0.1. По формуле C=2*π*r выходит ответ: 0.628
